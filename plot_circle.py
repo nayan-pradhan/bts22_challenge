3b9770c21ea4589310343aeebdf8026f1c39e732
Calin Clichici <calin.clichici@gmail.com>
2022-09-27 16:40:45 +0200

    updated plot_circle, now plotting 3 circles

--- plot_circle.py
+++ plot_circle.py
@@ -1,11 +1,18 @@
-
 import matplotlib.pyplot as plt
 
+xA, yA, rA = 35.4, 23.8, 5
+xB, yB, rB = 30.5, 20.4, 5
+xC, yC, rC = 32.4, 27.2, 5
 fig, ax = plt.subplots()
-draw_circle = plt.Circle( (35.4, 23.8), 0.3, fill = False)
-ax.set_xlim((34.8, 36))
-ax.set_ylim((23.2, 24.4))
-ax.add_patch(draw_circle)
+A = plt.Circle( (xA, yA), rA, fill = False)
+B = plt.Circle( (xB, yB), rB, fill = False)
+C = plt.Circle( (xC, yC), rC, fill = False)
+
+ax.set_xlim((15, 45))
+ax.set_ylim((12, 35))
+ax.add_patch(A)
+ax.add_patch(B)
+ax.add_patch(C)
 
 plt.title('Circle Drawing')
 plt.show()

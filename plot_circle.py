fd2c226114d5162be2d072b537121a4d026338b8
Calin Clichici <calin.clichici@gmail.com>
2022-09-27 17:51:11 +0200

    added get_intersections script to find intersections between 2 circles and updated plot_circle accordingly to plot 3 circles with intersections

--- plot_circle.py
+++ plot_circle.py
@@ -1,18 +1,47 @@
 import matplotlib.pyplot as plt
+import get_intersections as intersec
+import math
 
-xA, yA, rA = 35.4, 23.8, 5
-xB, yB, rB = 30.5, 20.4, 5
-xC, yC, rC = 32.4, 27.2, 5
+
+xA, yA, rA = 10, 10, math.sqrt(500)
+xB, yB, rB = 30, 5, math.sqrt(725)
+xC, yC, rC = 40, 40, math.sqrt(500)
+
 fig, ax = plt.subplots()
+
 A = plt.Circle( (xA, yA), rA, fill = False)
 B = plt.Circle( (xB, yB), rB, fill = False)
 C = plt.Circle( (xC, yC), rC, fill = False)
 
-ax.set_xlim((15, 45))
-ax.set_ylim((12, 35))
-ax.add_patch(A)
-ax.add_patch(B)
-ax.add_patch(C)
+ax.set_xlim((-40, 100))
+ax.set_ylim((-40, 100))
+ax.add_artist(A)
+ax.add_artist(B)
+ax.add_artist(C)
 
-plt.title('Circle Drawing')
+# xAB_1, yAB_1, xAB_2, yAB_2 = intersec.get_intersections(xA, yA, rA, xB, yB, rB)
+# xAC_1, yAC_1, xAC_2, yAC_2 = intersec.get_intersections(xA, yA, rA, xC, yC, rC)
+# xBC_1, yBC_1, xBC_2, yBC_2 = intersec.get_intersections(xB, yB, rB, xC, yC, rC)
+
+# plt.plot([xAB_1, yAB_1], [xAB_2, yAB_2], '.', color='r')
+# plt.plot([xAC_1, yAC_1], [xAC_2, yAC_2], '.', color='r')
+# plt.plot([xBC_1, yBC_1], [xBC_2, yBC_2], '.', color='r')
+
+intersections = intersec.get_intersections(xA, yA, rA, xB, yB, rB)
+if intersections is not None:
+    x1, y1, x2, y2 = intersections
+    plt.plot([x1, x2], [y1, y2], '.', color='r')
+
+intersections = intersec.get_intersections(xA, yA, rA, xC, yC, rC)
+if intersections is not None:
+    x1, y1, x2, y2 = intersections
+    plt.plot([x1, x2], [y1, y2], '.', color='r')
+
+intersections = intersec.get_intersections(xB, yB, rB, xC, yC, rC)
+if intersections is not None:
+    x1, y1, x2, y2 = intersections
+    plt.plot([x1, x2], [y1, y2], '.', color='r')
+
+
+plt.gca().set_aspect('equal', adjustable='box')
 plt.show()
